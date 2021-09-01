dbdf7ba6493fde2ef2a8cde05977b40d667d948e
adriell <adriell85>
2021-09-01 16:46:16 -0300

    created the folder structure for input and output

--- authSegmentation.py
+++ authSegmentation.py
@@ -1,7 +1,7 @@
 from numba import njit
 import cv2
-# from pynput.mouse import Controller
 import numpy as np
+import glob
 
 
 def kmeans_segmentation(img, K):
@@ -21,74 +21,13 @@ def kmeans_segmentation(img, K):
 
     return res2
 
-# @njit
-# def regionGrowing(img, object, xd, yd):
-#
-#     rows, cols = img.shape[:2]
-#     inicio=0
-#     fim=1
-#     while(inicio!=fim):
-#         inicio=fim
-#         fim=0
-#         for row in range(1,rows-1):
-#             for col in range(1,cols-1):
-#                 if object[row,col]==255:
-#                     for i in range(-1,2):
-#                         for j in range(-1,2):
-#                             if (img[row+i,col+j]<(img[xd,yd])+50)&(img[row+i,col+j]>(img[xd,yd])-50):
-#                                 object[row+i,col+j]=255
-#                                 # colors[row+i,col+j]=img[row+i,col+j]
-#                                 fim+=1
-#     return object
-#
-# # mouse = Controller()
-# # mouse.position
-#
-# point_matrix = np.zeros((2, 2), int)
-#
-#
-#
-# def mousePoints(event, x, y, flags, params):
-#     global point_matrix
-#     if event == cv2.EVENT_LBUTTONDOWN:
-#         point_matrix = x, y
-#         img2 = regionGrowing(img, np.zeros((img.shape[1], img.shape[0])), point_matrix[1], point_matrix[0])
-#         cv2.imshow('test', img2)
-#
-#
-#
-img = cv2.imread('output.jpg',1)
-scale_percent = 20  # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-
-
-img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-orig = img
-img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-# img = kmeans_segmentation(img,3)
-
-
-# cv2.imshow('img',img)
-#
-# cv2.setMouseCallback("img",mousePoints)
-
-# cv2.waitKey()
-
-
-
-
 def region_growing_gray(img):
     rows, cols = img.shape[:2]
     objeto = np.zeros((rows, cols), np.uint8)
     colors = np.zeros((rows, cols), np.uint8)
     rows_new = int(rows / 1)
     cols_new = int(cols / 1)
-    # img = cv2.resize(img, (rows_new, cols_new))
-    # objeto = cv2.resize(objeto, (rows_new, cols_new))
-    # colors = cv2.resize(colors, (rows_new, cols_new))
+
 
 
 
@@ -98,20 +37,19 @@ def region_growing_gray(img):
             global yd
             xd = x
             yd = y
-            # objeto=region_growing_binary(img)
+
             objeto[xd, yd] = 255
-            print(xd)
-            print(yd)
+
 
 
 
     cv2.imshow('imagem', img)
     cv2.namedWindow('imagem', 1)
     cv2.setMouseCallback('imagem', Mouse_click)
-    # objeto[xd, yd] = 255
+
     cv2.waitKey(0)
 
-    # return cv2.resize(iterations_growing_gray(img,objeto,colors),(rows,cols))
+
     return iterations_growing_gray(img,objeto,colors)
 
 @njit
@@ -128,29 +66,48 @@ def iterations_growing_gray(img,objeto,colors):
                 if objeto[row,col]==255:
                     for i in range(-1,2):
                         for j in range(-1,2):
-                            if (img[row+i,col+j]<(img[xd,yd])+80)&(img[row+i,col+j]>(img[xd,yd])-80):
+                            if (img[row+i,col+j]<(img[xd,yd])+20)&(img[row+i,col+j]>(img[xd,yd])-20):
                                 objeto[row+i,col+j]=255
                                 colors[row+i,col+j]=img[row+i,col+j]
                                 fim+=1
-                    # cv2.imshow('colors',colors)
-                    # cv2.waitKey(1)
+
 
     return colors
 
-img = region_growing_gray(img)
 
-gray = cv2.bilateralFilter(img, 11, 17, 17)
-Blur=cv2.GaussianBlur(gray,(5,5),1)
-edged = cv2.Canny(Blur, 10, 250)
-contours, hierarchy = cv2.findContours(edged,
-                                           cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-cv2.drawContours(orig, contours, -1, (0, 255, 0), 3)
-
-black = np.zeros((width,height,3))
-black = cv2.resize(black, dim, interpolation=cv2.INTER_AREA)
-cv2.drawContours(black, contours, -1, (0, 255, 0), 3)
-
-cv2.imshow('black',black)
-
-cv2.imshow('w',orig)
-cv2.waitKey(0)
+for name in glob.glob('region_growing_imgs_tests/*'):
+    print(name)
+    img = cv2.imread(name,1)
+    original_width = int(img.shape[0])
+    original_height = int(img.shape[1])
+    scale_percent = 20  # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    orig = img
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = region_growing_gray(img)
+    gray = cv2.bilateralFilter(img, 11, 17, 17)
+    Blur=cv2.GaussianBlur(gray,(5,5),1)
+    edged = cv2.Canny(Blur, 10, 250)
+    contours, hierarchy = cv2.findContours(edged,
+                                               cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(orig, contours, -1, (0, 255, 0), 3)
+    black = np.zeros((width,height,3),dtype=np.uint8)
+    black = cv2.resize(black, dim, interpolation=cv2.INTER_AREA)
+    cv2.drawContours(black, contours, -1, (0, 255, 0), 2)
+    bl = cv2.cvtColor(black,cv2.COLOR_BGR2GRAY)
+    contours,hierarchy =cv2.findContours(bl,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    max_value_contour = []
+    for contour in contours:
+        if len(contour) > len(max_value_contour):
+            max_value_contour = contour
+    rect = cv2.minAreaRect(max_value_contour)
+    box = cv2.boxPoints(rect)
+    box = box.astype('int')
+    img_box_2 = cv2.drawContours(black, contours = [box],
+                                 contourIdx = -1,  color = (0, 0, 255), thickness = 2)
+    print(box)
+    cv2.imshow('black',black)
+    cv2.waitKey(0)
--- scanner_test.py
+++ scanner_test.py
@@ -1,79 +1,87 @@
 import cv2
-import matplotlib.pyplot as ppl
+import glob
 import numpy as np
 from skimage.segmentation import flood, flood_fill
 
-color = cv2.imread('cnh1.jpg',1)
-img = cv2.cvtColor(color,cv2.COLOR_BGR2GRAY)
-original_width = int(img.shape[0])
-original_height = int(img.shape[1])
-scale_percent = 60  # percent of original size
-width = int(img.shape[0] * scale_percent / 100)
-height = int(img.shape[1] * scale_percent / 100)
-dim = (width, height)
-img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-color = cv2.resize(color,dim,interpolation=cv2.INTER_AREA)
-binary_img = np.where(img > 250,255,0)
-binary_img = np.uint8(binary_img)
+for name in glob.glob('input_scanner/*'):
+    image_name = name.split('/')[1]
+    color = cv2.imread(name,1)
+    img = cv2.cvtColor(color,cv2.COLOR_BGR2GRAY)
+    original_width = int(img.shape[0])
+    original_height = int(img.shape[1])
+    scale_percent = 60  # percent of original size
+    width = int(img.shape[0] * scale_percent / 100)
+    height = int(img.shape[1] * scale_percent / 100)
+    dim = (height, width)
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    color = cv2.resize(color,dim,interpolation=cv2.INTER_AREA)
+    binary_img = np.where(img > 250,255,0)
+    binary_img = np.uint8(binary_img)
 
-kernel = np.ones((7,7),np.uint8)
+    kernel = np.ones((7,7),np.uint8)
 
-closing = cv2.morphologyEx(binary_img, cv2.MORPH_CLOSE, kernel)
+    closing = cv2.morphologyEx(binary_img, cv2.MORPH_CLOSE, kernel)
 
-canny = cv2.Canny(closing,10,50)
-black = np.zeros((width,height,3),dtype=np.uint8)
+    canny = cv2.Canny(closing,10,50)
+    black = np.zeros((width,height,3),dtype=np.uint8)
 
-black = cv2.resize(black,dim,interpolation=cv2.INTER_AREA)
-
-
-
-contours,hierarchy =cv2.findContours(binary_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-
-cv2.drawContours(black,contours,-1,(0,255,0),2)
+    black = cv2.resize(black,dim,interpolation=cv2.INTER_AREA)
 
 
 
-bl = cv2.cvtColor(black,cv2.COLOR_BGR2GRAY)
-
-rows,cols = bl.shape[:2]
-
-for row in range(rows):
-    for col in range(cols):
-        if (row<200 and col < cols):
-            bl[row][col] = 0
-        elif(row<rows and col<50):
-            bl[row][col] = 0
-        else:
-            bl[row][col] = bl[row][col]
-
-light_coat = flood_fill(bl, (155, 150), 255, tolerance=10)
-
-light_coat = np.uint8(np.where(light_coat==0,255,0))
-
-light_coat = cv2.morphologyEx(light_coat, cv2.MORPH_OPEN, kernel)
-
-light_coat = cv2.resize(light_coat,(original_height,original_width),interpolation=cv2.INTER_AREA)
-
-contours,hierarchy =cv2.findContours(light_coat,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-max_value_contour = []
-for contour in contours:
-    if len(contour) > len(max_value_contour):
-        max_value_contour = contour
+    contours,hierarchy =cv2.findContours(binary_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 
-rect = cv2.minAreaRect(max_value_contour)
-box = cv2.boxPoints(rect)
-box = box.astype('int')
+    cv2.drawContours(black,contours,-1,(0,255,0),2)
 
-img_output = np.zeros((original_width,original_height,3),dtype=np.uint8)
 
-img_box_2 = cv2.drawContours(img_output, contours = [box],
-                             contourIdx = -1,  color = (0, 0, 255), thickness = 2)
 
-print(box)
-cv2.imshow('output',img_box_2)
+    bl = cv2.cvtColor(black,cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite('output.jpg',img_box_2)
-cv2.waitKey(0)
+    rows,cols = bl.shape[:2]
+
+    for row in range(rows):
+        for col in range(cols):
+            if (row<200 and col < cols):
+                bl[row][col] = 0
+            elif(row<rows and col<50):
+                bl[row][col] = 0
+            else:
+                bl[row][col] = bl[row][col]
+
+    light_coat = flood_fill(bl, (155, 150), 255, tolerance=10)
+
+    light_coat = np.uint8(np.where(light_coat==0,255,0))
+
+    light_coat = cv2.morphologyEx(light_coat, cv2.MORPH_OPEN, kernel)
+
+    light_coat = cv2.resize(light_coat,(original_height,original_width),interpolation=cv2.INTER_AREA)
+
+
+    contours,hierarchy =cv2.findContours(light_coat,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    max_value_contour = []
+    for contour in contours:
+        if len(contour) > len(max_value_contour):
+            max_value_contour = contour
+
+
+    rect = cv2.minAreaRect(max_value_contour)
+    box = cv2.boxPoints(rect)
+    box = box.astype('int')
+
+    img_output = np.zeros((width,height,3),dtype=np.uint8)
+
+    img_output = cv2.resize(img_output,(original_height,original_width),interpolation=cv2.INTER_AREA)
+
+    img_box_2 = cv2.drawContours(img_output, contours = [box],
+                                 contourIdx = -1,  color = (0, 0, 255), thickness = 2)
+
+
+
+    print(box)
+
+
+
+    cv2.imwrite(f'output_scanner/{image_name}',img_box_2)
+    cv2.waitKey(0)
